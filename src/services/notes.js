38e6fbf9f15fb7945c6f5e95a851bf95ab4745e1
carlos andres carandino <andrescarandino5@gmail.com>
2025-08-22 15:37:34 -0300

    change service notes

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,26 +1,14 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes';
+const baseUrl = '/api/notes';
 
-/* const getAll = () => {
-    return axios.get(baseUrl)
-} */
 const getAll = () => {
     const request = axios.get(baseUrl);
     return request.then(response => response.data);
 }
 
-/* const create = newObject => {
-    return axios.post(baseUrl, newObject)
-} */
-
 const create = newObject => {
     const request = axios.post(baseUrl, newObject);
     return request.then(response => response.data);
-}
-
-/* const update = (id, newObject) => {
-    return axios.put(`${baseUrl}/${id}`, newObject)
-} */
 
 const update = (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject);
